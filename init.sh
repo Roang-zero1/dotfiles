b8abdd8c473056dade53d4060533663ea515b89a
Lucas Brandstaetter <lucas@brandstaetter.tech>
2016-05-04 09:55:06 +0200

    Add new zsh autostart
    Reformat init.sh for better readability

--- init.sh
+++ init.sh
@@ -2,34 +2,35 @@
 
 cd ~
 
-# Initialize zsh
-echo "Relinking .zshrc"
+# Default linking
+echo "Linking defaults"
 rm -f .zshrc
-ln -s ~/tools/zsh/zshrc .zshrc
-
-echo "Relinking .gitconfig"
-rm -f .gitconfig                                                                                                                                   
-case $HOSTNAME in
-  (canopus.uberspace.de) echo "Linkink userspace";ln -s ~/tools/git/uberspace .gitconfig;;
-  (*) ;;
-esac
-
-# Initialize gem local environment
-echo "Relinking .gemrc"
-rm -f .gemrc
-case $HOSTNAME in
-  (canopus.uberspace.de) echo "Linkink userspace";ln -s ~/tools/ruby/gemrc-userinstall .gemrc;;
-  (*) ;;
-esac
-
-# Initialize vim
-echo "Relinking .vimrc"
+rm -f .gitconfig
 rm -f .vimrc
-ln -s ~/tools/vim/vimrc .vimrc
-
-mkdir -p .vim
 rm -f .vim/bundle
-ln -s ~/tools/vim/bundle .vim/bundle
 rm -f .vim/plugin
+
+echo "Relinking .zshrc"
+ln -s ~/tools/zsh/zshrc .zshrc
+echo "Relinking .gitconfig"
+ln -s ~/tools/git/uberspace .gitconfig
+echo "Relinking vim files and dirs"
+ln -s ~/tools/vim/vimrc .vimrc
+mkdir -p .vim
+ln -s ~/tools/vim/bundle .vim/bundle
 ln -s ~/tools/vim/plugin .vim/plugin
 
+# Host specific linking
+case $HOSTNAME in
+  (canopus.uberspace.de)
+      echo "Linkink userspace";
+      rm -f .bash_profile
+      rm -f .gemrc
+      
+      echo "Relinking .gemrc"
+      ln -s ~/tools/ruby/gemrc-userinstall .gemrc
+      echo "Relinking bash_profile"
+      ln -s ~/tools/bash/bash_profile_uberspace .bash_profile
+      ;;
+  (*) ;;
+esac
