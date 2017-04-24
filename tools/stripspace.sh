2561d99ea1e1272479ca88e1de54595f22becd9d
Lucas Brandstaetter <lucas@brandstaetter.tech>
2017-04-24 23:58:36 +0200

    Remove defunct stripspace function

--- tools/stripspace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -not \( -path "*/.git" -prune \) -not -name "*.md" -type f | while read file ; do
-  echo "Stripspaces from $file"
-  git stripspace < "$file" | sponge "$file"
-done
