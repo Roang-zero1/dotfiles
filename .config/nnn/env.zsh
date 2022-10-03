491d8cf8093a64009090ca6d0f8c9a8c1d57a935
Lucas Brandstaetter <lbr@limessecurity.com>
2022-10-03 16:27:52 +0200

    Update nnn config
    * Download plugins
    * Add configuration environment variables

---
+++ .config/nnn/env.zsh
@@ -0,0 +1,5 @@
+# NNN Configuration is done via environment variables
+export NNN_PLUG='o:fzopen;p:preview-tui;d:dups;c:diffs;f:fzopen;s:finder'
+
+export NNN_FCOLORS='0000DDCA0000000000000000'
+export NNN_FIFO='/tmp/nnn.fifo'
