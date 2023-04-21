07ef1a84c0aff581914c3c5260bee830c5a67f25
Lucas Brandstaetter <lbr@limessecurity.com>
2023-04-21 14:03:01 +0200

    Add suport for VSCode interactive terminals

--- .p10k.zsh
+++ .p10k.zsh
@@ -1481,6 +1481,8 @@
   # really need it.
   typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
 
+  [[ "$TERM_PROGRAM" == "vscode" ]] && typeset -g POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
+
   # If p10k is already loaded, reload configuration.
   # This works even with POWERLEVEL9K_DISABLE_HOT_RELOAD=true.
   (( ! $+functions[p10k] )) || p10k reload
