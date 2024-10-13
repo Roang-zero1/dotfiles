16a667e332bcd4d67cb9a947b859c2bd1053f31f
Lucas Brandstaetter <lucas@brandstaetter.tech>
2024-10-13 13:03:11 +0000

    Add password change

--- scripts/download-ssh-key.py
+++ scripts/download-ssh-key.py
@@ -2,13 +2,14 @@
 import json
 from os import environ
 from pathlib import Path
-from subprocess import PIPE, Popen, check_output
+from subprocess import PIPE, Popen
 
 import boto3
 from botocore.exceptions import ClientError
 
 
 def get_secret():
+    username = environ.get("CLOUD_INIT_USERNAME", "lucas")
     secret_name = environ.get("SECRET_NAME", "ssh_key")
     region_name = "eu-central-1"
 
@@ -38,6 +39,22 @@ def get_secret():
 
         with Path("/home/lucas/.ssh/id_ed25519").open("w") as of:
             of.write(secrets["key"].replace("\\n", "\n"))
+
+        if "password" in secrets:
+            p = Popen(
+                ["/usr/sbin/chpasswd"],
+                universal_newlines=True,
+                shell=False,
+                stdin=PIPE,
+                stdout=PIPE,
+                stderr=PIPE,
+            )
+            (stdout, stderr) = p.communicate(
+                username + ":" + secrets["password"] + "\n"
+            )
+            p.wait()
+            if stdout or stderr:
+                raise Exception("Error encountered changing the password!")
 
         print(secrets["passphrase"])
 
